02e199d495cd49347031ace5c9a0f1e87a7f9a09
Innocent Ingabire <inno100os@gmail.com>
2023-02-16 12:54:19 -0800

    new changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/**main function*/
+/**
+ * main -Print a line of code using printf
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main -Print size of datatypes
+ * Return: 0
+ *
+ */
 
 int main(void)
 {
