6c8b3f9ba40e7efeffcfa07018c0839247417add
Innocent Ingabire <inno100os@gmail.com>
2023-02-21 08:00:21 -0800

    three and four

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks alphabetical character
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90)||(c <= 122 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
