512b6fc470f1705674b4720e5c9500135bbfcd48
Innocent Ingabire <inno100os@gmail.com>
2023-02-21 08:21:17 -0800

    correct 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - checks alphabetical character
+ * @c: passing character as a parameter
  * Return: 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90)||(c <= 122 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c <= 122 && c <= 122))
 	{
 		return (1);
 	}
