1fa4fdff184f3cebaa692882afb9bb4fc230e4f2
Innocent Ingabire <inno100os@gmail.com>
2023-02-22 02:27:03 -0800

    last

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,7 +20,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d, ", i);	
+				printf("%d, ", i);
 			}
 		}
 	}
@@ -30,14 +30,13 @@ void print_to_98(int n)
 
 		for (i = n; i >= 98; i--)
 		{
-			
 			if (i == 98)
 			{
 				printf("%d\n", i);
 			}
 			else
 			{
-				printf("%d, ", i);	
+				printf("%d, ", i);
 			}
 		}
 	}
