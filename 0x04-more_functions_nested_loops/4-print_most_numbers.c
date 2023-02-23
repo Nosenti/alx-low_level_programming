998ccc9923ca407f19f693d3b785e9051bb1473c
Innocent Ingabire <inno100os@gmail.com>
2023-02-23 11:16:14 -0800

    up to 6

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers from 0 to 1d4
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j = 0;
+
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
