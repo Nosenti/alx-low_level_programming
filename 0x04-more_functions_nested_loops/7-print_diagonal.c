043659a55fcfefea3d0c1bef0b15fc1ea265f1e7
Innocent Ingabire <inno100os@gmail.com>
2023-02-23 19:33:37 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: int parameter
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
