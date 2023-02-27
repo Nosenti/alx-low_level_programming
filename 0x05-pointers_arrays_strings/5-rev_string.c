6bb3016039c46ac0eff940a4d94774db588427e9
Innocent Ingabire <inno100os@gmail.com>
2023-02-27 14:50:20 -0800

    5 and 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length, i;
+	char temp;
+
+	length = _strlen(s);
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
