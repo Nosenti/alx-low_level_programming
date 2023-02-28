57d2782682b5da7c61134d83ecb8e88260dd3c45
Innocent Ingabire <inno100os@gmail.com>
2023-02-27 16:41:37 -0800

    add min and max

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -34,7 +34,18 @@ int _atoi(char *s)
 		int digit;
 
 		digit = s[i] - '0';
-		result = result * 10 - digit;
+		if (result > (INT_MAX - digit) / 10)
+		{
+			if (sign == -1)
+			{
+				return (INT_MIN);
+			}
+			else
+			{
+				return (INT_MAX);
+			}
+		}
+		result = result * 10 + digit;
 		i++;
 	}
 	return (sign * result);
