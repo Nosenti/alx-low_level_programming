446139ea59cadba89f5c1afaee209b4fb50b5d38
Innocent Ingabire <inno100os@gmail.com>
2023-03-02 09:55:43 -0800

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, temp;
 
 	for (i = 0; i < n / 2; i++)
 	{
+		temp = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
+		a[n - 1 - i] = temp;
 	}
 }
