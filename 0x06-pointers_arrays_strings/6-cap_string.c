33e878f59db43bab694d21513bd3cd77e5b62d89
Innocent Ingabire <inno100os@gmail.com>
2023-03-02 19:56:37 -0800

    qn 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,6 +17,17 @@ char *cap_string(char *str)
 
 	for (i = 1; str[i] != '\0'; i++)
 	{
-		if ((str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' || str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.' || str[i-1] == '!'))
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
+					str[i - 1] == '\n' || str[i - 1] == ',' ||
+					str[i - 1] == ';' || str[i - 1] == '.' ||
+					str[i - 1] == '!' || str[i - 1] == '?' ||
+					str[i - 1] == '"' || str[i - 1] == '(' ||
+					str[i - 1] == ')' || str[i - 1] == '{' ||
+					str[i - 1] == '}')
+				&& (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
 	}
+	return (str);
 }
