0c6ae9006a566e7561c75d2ce2bdea69c2b97890
Innocent Ingabire <inno100os@gmail.com>
2023-03-02 19:33:34 -0800

    change 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] == s2[i])
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0')
 		{
-			return s1[i] - s2[i];
+			return (0);
 		}
+		i++;
 	}
 
-	return 0;
+	return (s1[i] - s2[i]);
 }
