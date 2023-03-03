2615c3fa9cdb76afb5994bcc7a005108453180ec
Innocent Ingabire <inno100os@gmail.com>
2023-03-02 20:05:26 -0800

    question 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * @str: string
+ * Return: string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char ltable[];
+
+	ltable = "aAeEoOtTlL44377011";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == ltable[j])
+			{
+				str[i] = ltable[j + 10];
+				break;
+			}
+		}
+	}
+	return (str);
+}
