4225f612617a11544915a261b1121b0094fc9ca8
Innocent Ingabire <inno100os@gmail.com>
2023-03-07 14:13:36 -0800

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *ptr = s;
 
-	for (i = 0; i < n; i++)
+	while (n-- > 0)
 	{
-		s[i] = b;
+		*ptr++ = b;
 	}
 	return (s);
 }
