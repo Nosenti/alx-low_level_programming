62adfeb992e8e06659322014dabf3af6d5cfac0e
Innocent Ingabire <inno100os@gmail.com>
2023-03-07 15:48:31 -0800

    change 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,15 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int r, i;
 
-	for (i = 0; i < n; i++)
+	r = 0;
+	i = n;
+
+	for (; r < i; r++)
 	{
-		dest[i] = *src;
+		dest[r] = src[r];
+		n--;
 	}
 	return (dest);
 }
