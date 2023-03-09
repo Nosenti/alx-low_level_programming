27b78783ab016490e04e785b556576641836e446
Innocent Ingabire <inno100os@gmail.com>
2023-03-08 19:41:59 -0800

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,26 +8,25 @@
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	if (n == 2)
-	{
-		return (1);
-	}
-	return (is_prime_helper(n, 2));
+	return (is_prime_number(n, n - 1));
 }
 
-int is_prime_helper(int n, int divisor)
+int is_prime_number(int n, int divisor)
 {
-	if (n % divisor == 0)
+	if (n < 2)
 	{
 		return (0);
 	}
-	if (divisor * divisor > n)
+	else if (divisor == 0)
 	{
 		return (1);
 	}
-	return (is_prime_helper(n, divisor + 1));
+	else if (n % divisor == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_number(n, n - 1));
+	}
 }
