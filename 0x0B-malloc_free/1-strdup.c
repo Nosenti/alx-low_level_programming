b6127baa677f3c3ac2e3a2c9eadfc9a0b028499c
Innocent Ingabire <inno100os@gmail.com>
2023-03-13 14:38:37 -0700

    something on 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -26,12 +26,14 @@ char *_strdup(char *str)
 	}
 
 	cpy = malloc(sizeof(char) * len + 1);
-
+	if (cpy == NULL)
+	{
+		return NULL;
+	}
 	for (i = 0; i < len; i++)
 	{
 		cpy[i] = str[i];
 	}
-	cpy[len] = '\0';
 	return (cpy);
 
 }
