dd522c35f1654e9d10691f683da9051c2446e757
Innocent Ingabire <inno100os@gmail.com>
2023-03-14 10:27:24 -0700

    args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the args
+ * @ac: number of args
+ * @av: args array
+ * Return: pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, len;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	k = 0;
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	str = (char *)malloc(len * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		str[k++] = '\n';
+	}
+	str[k] = '\0';
+	return (str);
+}
