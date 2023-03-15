fd87be9747b86b74f03c7319f6870c2a91bf6189
Innocent Ingabire <inno100os@gmail.com>
2023-03-15 02:32:22 -0700

    update

--- 0x09-static_libraries/create-static_lib.sh
+++ 0x09-static_libraries/create-static_lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+C_FILES=$(find . -name "*.c")
+for file in $C_FILES; do
+	gcc -c $file
+done
+ar rcs liball.a *.o
+rm *.o
