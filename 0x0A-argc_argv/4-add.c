28402f34ddc9f92d8ba2393a9c8cbc0c943d9d4e
Innocent Ingabire <inno100os@gmail.com>
2023-03-15 11:14:17 -0700

    number 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - add numbers
@@ -10,28 +11,21 @@
 
 int main(int argc, char *argv)
 {
-	int i, sum;
-	
+	int i, j, sum;
+
 	sum = 0;
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!atoi(argv[i]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum = sum + atoi(argv[i]);
-			}
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
