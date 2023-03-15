892e30c0b2a7abcecefc55763303589e069ae9f8
Innocent Ingabire <inno100os@gmail.com>
2023-03-15 10:32:18 -0700

    number 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - print args count
+ * @argc: args count
+ * @argv: args
+ * Return: int
+ */
+
 int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d", argc);
 	printf("\n");
+	return (0);
 }
