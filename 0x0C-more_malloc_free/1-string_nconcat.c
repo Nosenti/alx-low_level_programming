941405431cb227fbdc167b785844c7a9f51b7fd6
Innocent Ingabire <inno100os@gmail.com>
2023-03-16 18:03:29 -0700

    number 1 update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "main.h"
 #include "2-strlen.c"
 
@@ -12,34 +14,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	char *newstr;
-
+	unsigned int s2len, s1len;
+	
+	s1len = UINT_MAX + 1 + _strlen(s1);
+	s2len = UINT_MAX + 1 + _strlen(s2);
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1len = 0;
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2len = 0;
 	}
-	if (n >= _strlen(s2))
+	if (n >= s2len)
 	{
-		n = _strlen(s2);
+		n = s2len;
 	}
-	newstr = malloc(sizeof(char) * _strlen(s1) + n + 1);
+	newstr = malloc(sizeof(char) * s1len + n + 1);
 	if (!newstr)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != "\0"; i++)
+	for (i = 0; i < s1len; i++)
 	{
 		newstr[i] = s1[i];
 	}
-	for (i = _strlen(s1) - 1; i < _strlen(s1) + n; i++)
+	for (i = s1len - 1; i < s1len + n; i++)
 	{
-		newstr[i] = s2[i -_ strlen(s1)];
+		newstr[i] = s2[i - s1len];
 	}
-	newstr[_strlen(s1) + n] = '\0';
+	newstr[s1len + n] = '\0';
 	return (newstr);
 }
