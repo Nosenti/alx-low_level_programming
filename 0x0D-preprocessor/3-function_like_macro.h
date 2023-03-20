606d3d6faecd78dd056e30c52abf41404d4e0441
Innocent Ingabire <inno100os@gmail.com>
2023-03-20 15:26:02 -0700

    avoid double inclusion

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,3 @@
-#ifndef ABS
-/*
- * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
+#ifndef ABS(x)
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #endif
