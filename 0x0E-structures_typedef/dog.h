8d72204f0f8a572911b1d69969cf73cf4ba17af9
Innocent Ingabire <inno100os@gmail.com>
2023-03-20 13:19:03 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: A dog struct represents a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
