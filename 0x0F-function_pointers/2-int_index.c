20f8e49cbcdb348ac2b36b92bb998c5d03c9e663
Innocent Ingabire <inno100os@gmail.com>
2023-03-22 15:49:50 -0700

    include edge cases for when array does not exist

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,27 +1,27 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: given array of integers
- * @size: size of the array
- * cmp: cmp function
- * Return int
+ * int_index - Searches for an integer in an array of integers.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @cmp: A pointer to the function to be used to compare values.
+ *
+ * Return: If no element matches or size <= 0 - -1.
+ *         Otherwise - The index of the first element for which
+ *                     the cmp function does not return 0.
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int index;
 
-	if (size <= 0)
-	{
+	if (array == NULL || cmp == NULL)
 		return (-1);
-	}
-	for (i = 0; i < size; i++)
+
+	for (index = 0; index < size; index++)
 	{
-		if ((*cmp)(array[i]) != 0)
-		{
-			return (i);
-		}
+		if (cmp(array[index]) != 0)
+			return (index);
 	}
+
 	return (-1);
 }
