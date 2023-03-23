0223d9c265fa61c0901b522a5feb5539af8254ca
Innocent Ingabire <inno100os@gmail.com>
2023-03-23 07:41:26 -0700

    betty compliance

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of args passed to the func
+ * @...: params
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	const char *p;
+	char c;
+	int i;
+	float f;
+	char *s;
+	va_list args;
+
+	p = format;
+	va_start(args, format);
+	while (*p)
+	{
+		switch (*p)
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = var_arg(args, double);
+				printf("%f", f);
+				break;
+			case "s":
+				s = va_arg(args, char *);
+				if (!s)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				i++;
+				continue;
+
+		}
+		p++;
+	}
+	printf("\n");
+	va_end(args);
+}
