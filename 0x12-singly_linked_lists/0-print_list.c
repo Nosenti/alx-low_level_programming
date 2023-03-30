d98c5a923b11f294620d91f14fdd011f61b7ec29
Innocent Ingabire <inno100os@gmail.com>
2023-03-30 08:57:37 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_list - prints elements of a linkedlist
+ * @h: head of a linkedlist
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%zu] %s\n", len, h->str);
+		}
+		else
+		{
+			printf("[%zu] (nil)\n", len);
+		}
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
