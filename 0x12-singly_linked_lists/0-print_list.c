80d3c0698912af3d76a96f75bc3ca0dd23e0f5a2
Innocent Ingabire <inno100os@gmail.com>
2023-03-30 09:34:29 -0700

    remove space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,15 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!(h -> str))
+		if (!(h->str))
 		{
-			printf("[0] (nil)\n");
+			printf("[%i] (nil)\n", h->len);
 		}
 		else
 		{
-			printf("[%i] %s\n", h -> len, h -> str);
+			printf("[%i] %s\n", h->len, h->str);
 		}
-		h = h -> next;
+		h = h->next;
 		len++;
 	}
 	return (len);
