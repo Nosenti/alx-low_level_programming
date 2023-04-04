3c166595b9ecdc7b2b4f61bd33de221d08f36073
Innocent Ingabire <inno100os@gmail.com>
2023-04-04 08:59:18 -0700

    number 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,19 +10,16 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-
 	listint_t *currNode = head;
-	while(count < index)
+
+	while(currentNode != NULL)
 	{
-		currNode = currNode->next;
+		if (count == index)
+		{
+			return currentNode;
+		}
+		currentNode = currentNode->next;
+		count++;
 	}
-	if (currNode->next == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		currNode = currNode->next;
-		return (currNode)
-	}
+	return (NULL);
 }
