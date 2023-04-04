565df668a5f203ade202c3b920f94ce958ce9fdb
Innocent Ingabire <inno100os@gmail.com>
2023-04-04 08:22:34 -0700

    number 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the list
+ * @head: head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *currNode;
+	listint_t *next;
+
+	currNode = *head;
+
+	if (currNode == NULL)
+	{
+		return;
+	}
+	while (currNode != NULL)
+	{
+		next = currNode->next;
+		free(currNode);
+		currNode = next;
+	}
+	*head = NULL;
+}
