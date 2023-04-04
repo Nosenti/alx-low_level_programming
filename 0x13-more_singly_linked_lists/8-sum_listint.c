5b0ddd8e4ab5343e3e75acd6b70c2e354e377b70
Innocent Ingabire <inno100os@gmail.com>
2023-04-03 23:47:13 -0700

    question number 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of elements in a LinkedList
+ * @head: head node
+ * Return: sum of elements
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!(head))
+	{
+		return (0);
+	}
+	listint_t *currNode = head;
+	while(currNode != NULL)
+	{
+		sum = sum + currNode->n;
+		currNode = currNode->next;
+	}
+	return (sum);
+}
