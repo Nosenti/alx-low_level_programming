79404d3970584371d99720fa76aa49ba31e14dd2
Innocent Ingabire <inno100os@gmail.com>
2023-04-11 13:31:34 -0700

    check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int test;
+	char *test_as_bytes;
+
+	test = 1;
+	test_as_bytes = (char *)&test;
+
+	return (test_as_bytes[0] == 1 ? 1 : 0);
+}
